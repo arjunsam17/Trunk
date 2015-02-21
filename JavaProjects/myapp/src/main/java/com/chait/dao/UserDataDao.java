9d3bd56437fc0165e35715de65af07f0d777fdb0
chait <chait@chait.com>
2015-02-21 14:47:13 -0500

    Remove unusued packages.

--- JavaProjects/myapp/src/main/java/com/chait/dao/UserDataDao.java
+++
@@ -1,9 +0,0 @@
-package com.chait.dao;
-
-import com.chait.domain.UserData;
-
-public interface UserDataDao {
-
-	UserData getUserDataById(int id);
-
-}
--- JavaProjects/myapp/src/main/java/com/chait/form/indexForm.java
+++
@@ -1,5 +0,0 @@
-package com.chait.form;
-
-public class indexForm {
-
-}
